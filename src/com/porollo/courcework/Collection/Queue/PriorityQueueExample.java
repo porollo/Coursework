2b96ba99172dbe2b407bdfa52dc1e14ce1028359
dporollo <d.porollo@gmail.com>
2015-11-25 01:11:31 +0300

    Final Update

--- src/com/porollo/courcework/Collection/Queue/PriorityQueueExample.java
+++ src/com/porollo/courcework/Collection/Queue/PriorityQueueExample.java
@@ -1,8 +1,31 @@
 package com.porollo.courcework.Collection.Queue;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Created by Dmitriy S. Porollo on 24.11.2015.
  */
 
 public class PriorityQueueExample {
+
+    public static void main(String[] args) {
+
+        // Создание экземпляра PriorityQueue
+        PriorityQueue< Integer > priorityQueue = new PriorityQueue <> ();
+
+        // Добавление элементов в очередь
+        priorityQueue.add(5);
+        priorityQueue.add(7);
+        priorityQueue.add(5);
+        priorityQueue.add(54);
+        priorityQueue.add(7);
+
+        System.out.println ( "Priority queue values: "+ priorityQueue);
+
+        // Создание компаратора
+        Comparator comparator = priorityQueue.comparator();
+
+        System.out.println ( "Comparator value: "+ comparator);
+    }
 }
--- src/com/porollo/courcework/Map/LinkedHashMapExample.java
+++ src/com/porollo/courcework/Map/LinkedHashMapExample.java
@@ -1,8 +1,9 @@
 package com.porollo.courcework.Map;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Dmitriy S. Porollo on 22.11.2015.
@@ -10,7 +11,32 @@ import java.util.Map;
 
 public class LinkedHashMapExample {
 
+    public static void main(String[] args) {
 
-
-
+        // Создание LinkedHashMap
+        LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap();
+        // Помещаем элементы в LinkedHashMap
+        linkedHashMap.put("Dima",   new Integer(9995));
+        linkedHashMap.put("Vasya",  new Integer(84));
+        linkedHashMap.put("Gleb",   new Integer(-1344));
+        linkedHashMap.put("Sergey", new Integer(85));
+        linkedHashMap.put("Andrey", new Integer(61));
+        // Поручение набора
+        Set set = linkedHashMap.entrySet();
+        // Создание итератора
+        Iterator iterator = set.iterator();
+        // Вывод элементов
+        while(iterator.hasNext()) {
+            Map.Entry me = (Map.Entry)iterator.next();
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+        System.out.println();
+        // Добавление 1000 к Dima
+        Integer newcount = linkedHashMap.get("Dima");
+        linkedHashMap.put("Dima", new Integer(newcount + 1000));
+        // Вывод результата
+        System.out.println("Dima newcount: " +
+                linkedHashMap.get("Dima"));
+    }
 }
